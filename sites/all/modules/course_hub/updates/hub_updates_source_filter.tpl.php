9a30160afca54f2a5a38c73d9fcf24ebc7bca872
Adam Franco <afranco@middlebury.edu>
2012-10-15 16:20:32 -0400

    Hub: Control statements should have one space between the control keyword and opening parenthesis

--- sites/all/modules/course_hub/updates/hub_updates_source_filter.tpl.php
+++ sites/all/modules/course_hub/updates/hub_updates_source_filter.tpl.php
@@ -1,5 +1,5 @@
 <select class="resource_filter" onchange="window.location=this.value;">
-<?php foreach($sources as $source): ?>
+<?php foreach ($sources as $source): ?>
   <option value="<?php print $source->filter_url; ?>" <?php if ($current_filter == $source->id) { print 'selected="selected" class="selected"'; } ?>>
   <?php print $source->title; ?> (<?php print $source->num_updates; ?>)
   </option>
