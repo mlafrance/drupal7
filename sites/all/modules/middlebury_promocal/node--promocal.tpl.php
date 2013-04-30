dbf0af93a5513ea3eedee13bbb4bc14649d50c66
Ian McBride <imcbride@middlebury.edu>
2013-04-30 08:55:35 -0400

    middlebury_promocal: Initial commit of migrated module.

---
+++ sites/all/modules/middlebury_promocal/node--promocal.tpl.php
@@ -0,0 +1,15 @@
+<article class="<?php print $classes; ?>">
+  <header>
+    <h1><?php print $title; ?></h1>
+  </header>
+  <script src="//25livepub.collegenet.com/scripts/spuds.js"></script>
+  <script>
+    $Trumba.addSpud({
+      webName: "<?php print $webname; ?>",
+      spudType: "<?php print $spudtype; ?>",
+      teaserBase: "<?php !empty($teaserbase) ? print $teaserbase : print ''; ?>"
+    });
+  </script>
+  <noscript>Your browser must support JavaScript to view this content. Please enable JavaScript in your browser settings then try again.</noscript>
+  <?php print render($content['links']); ?>
+</article>
