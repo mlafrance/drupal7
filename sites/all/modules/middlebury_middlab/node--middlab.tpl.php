239c4584ff4595f9ea684bef82b27362b03095dd
Ian McBride <imcbride@middlebury.edu>
2013-04-30 13:31:08 -0400

    middlebury_middlab: Initial Drupal 7 migration of this module.

---
+++ sites/all/modules/middlebury_middlab/node--middlab.tpl.php
@@ -0,0 +1,8 @@
+<article class="<?php print $classes . ' c' . rand(1, 14); ?>">
+  <header>
+    <h1><a href="<?php print $middlab_link; ?>"><?php print $middlab_title; ?></a></h1>
+  </header>
+  <section class="description"><?php print $middlab_description; ?></section>
+  <section class="department"><a href="<?php print $middlab_link; ?>">Discuss in Midd<b>Lab</b> &raquo;</a></section>
+</article>
+<?php print render($content['links']); ?>
