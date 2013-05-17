f51948931991b66fa0ebbda4015aa1300d061e8a
Ian McBride <imcbride@middlebury.edu>
2013-05-17 10:58:33 -0400

    midd: Initial commit of Middlebury site front page theme.

--- sites/all/themes/midd/page--front.tpl.php
+++ sites/all/themes/midd/page--front.tpl.php
@@ -1,18 +1,38 @@
 <?php print render($page['header']); ?>
 <?php include_once(drupal_get_path('theme', 'midd') . '/includes/header.inc'); ?>
-<article id="midd_content" class="pagecontent container">
-  <nav class="left blue">
-    <?php print render($page['left']); ?>
+<div id="midd_waveform" class="waveform">
+  <div class="slider">
+    <ul id="midd_stories" class="stories nojs">
+      <li class="disabled bar">
+        <div class="bar_contents">
+          <div class="bar_title">Welcome to Middlebury College</div>
+          <div class="bar_target">
+            <div class="bar_color">
+              <img src="<?php print $base_path . drupal_get_path('theme', 'midd'); ?>/images/bar_nojs.jpg" alt="Students on Midd's Vermont campus" width="360" height="220" />
+            </div>
+          </div>
+          <div class="bar_text">
+            Please <a href="http://www.google.com/support/bin/answer.py?answer=23852" target="_blank">enable Javascript in your browser</a> and <a href="<?php print $base_path; ?>">reload the page</a> to view interactive content.
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</div>
+<div class="container">
+  <?php include_once(drupal_get_path('theme', 'midd') . '/includes/navigation.inc'); ?>
+  <nav id="midd_affiliates" class="affiliates">
+    <h2>Programs &amp; Affiliates<span></span></h2>
+    <ul>
+      <li class="affiliates_languageschools"><a href="<?php print $base_path; ?>ls/">Middlebury Language Schools<span></span></a></li>
+      <li class="affiliates_schoolsabroad"><a href="<?php print $base_path; ?>sa/">Middlebury Schools Abroad<span></span></a></li>
+      <li class="affiliates_breadloaf"><a href="<?php print $base_path; ?>breadloaf/">Bread Loaf School of English &amp; Writers Conference<span></span></a></li>
+      <li class="affiliates_mmla"><a href="http://mmla.middlebury.edu/">Middlebury-Monterey Language Academy<span></span></a></li>
+      <li class="affiliates_miis"><a href="http://www.miis.edu/">Monterey Institute of International Studies<span></span></a></li>
+    </ul>
+    <div class="clear"></div>
   </nav>
-  <section class="page">
-    <?php print render($page['right']); ?>
-    <section class="body">
-      <?php print render($page['content']); ?>
-    </section>
-  </section>
-  <?php print render($page['address']); ?>
-  <div class="clear"></div>
-</article>
+</div>
 <?php print render($page['carousel']); ?>
 <?php include_once(drupal_get_path('theme', 'midd') . '/includes/footer.inc'); ?>
 <?php print render($page['footer']); ?>
--- sites/all/themes/midd/template.php
+++ sites/all/themes/midd/template.php
@@ -6,11 +6,20 @@
 function midd_preprocess_html(&$vars) {
   $args = arg();
   if (!empty($args[1])) {
-    $vars['classes_array'][] = middlebury_get_color_scheme($args[1]);
+    //$vars['classes_array'][] = middlebury_get_color_scheme($args[1]);
   }
 
   if ($vars['is_front']) {
     $vars['classes_array'][] = 'homepage';
     $vars['attributes_array']['id'] = 'midd_homepage';
+  }
+}
+
+/**
+ * Implements hook_preprocess_page().
+ */
+function midd_preprocess_page(&$vars) {
+  if ($vars['is_front']) {
+    drupal_add_js(base_path() . 'middlebury_story/get/Home');
   }
 }
